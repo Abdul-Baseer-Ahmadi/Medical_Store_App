e12c943ac1375d125c3c5886689ddb6a2a7f7349
Abdul Baseer Ahmadi <baseerahmadi33944@gmail.com>
2022-01-22 20:24:44 +0500

    Creating Test Report

---
+++ Medical_Store_App/Project Forms/TestReportForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Medical_Store_App.Project_Forms
+{
+    public partial class TestReportForm : Form
+    {
+        public TestReportForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
